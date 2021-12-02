cce7b982355ad23dc8ee35c79e608dd72c620f14
Alan-Rodz <s061199@hotmail.com>
2021-12-02 17:54:30 -0600

    third commit

--- blogProject/blogProject/settings.py
+++ blogProject/blogProject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts'
+
 ]
 
 MIDDLEWARE = [
@@ -47,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'posts'
 ]
 
 ROOT_URLCONF = 'blogProject.urls'
